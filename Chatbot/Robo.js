e31e03bea914b7c330a9bb0f6295db14c93a5593
J. Rodrigues <80002311+j-rodrigues-stack@users.noreply.github.com>
2021-04-22 04:33:02 -0300

    implemented simple answers functionalities

--- Chatbot/Robo.js
+++ Chatbot/Robo.js
@@ -1,21 +1,90 @@
-var Cliente = {
+window.onload = function getClientName() {
+    var nomeCliente = window.prompt("Por favor, antes de usar o nosso ChatBot, insira o seu nome:", "Anônimo");
+    
+    document.getElementById("cliente_nome").innerHTML = nomeCliente;
+}
 
-    Nome: "André Lucas",
+var Cliente = {
+    Nome: "",
     RealizarPergunta: function () {
         const pergunta = (document.getElementById('textarea_pergunta').value);
-        alert(`Pergunta enviada com sucesso.\n Sua pergunta: ${pergunta}`);
+        //alert(`Pergunta enviada com sucesso.\n Sua pergunta: ${pergunta}`);
+        BancoDeDados.ConsultarPergunta(pergunta);
+        Robo.ResponderPergunta();
     }
 };
 
-var Robo = {
+/*var Robo = {
     ResponderPergunta: function () {
-        var divResposta = document.getElementById("resposta_enviada");
         //divResposta.innerText = "Jean Rodrigues";
-        divResposta.innerText = document.getElementById("textarea_resposta").value;
-        alert("Resposta enviada com sucesso!");
+
+        //var dataHoje = new Date();
+        //console.log(dataHoje);
+        //alert(dataHoje);
+        //alert("Pergunta respondida");
     }
-};
+};*/
+
+var BancoDeDados = {
+
+    // Criei várias arrays para ter um dicionário de várias perguntas, separando em perguntas relacioandas a data, tempo, etc.
+
+    PerguntasData: [
+        { Pergunta: "Qual o dia de hoje?", Resposta: new Date().toLocaleDateString() },
+        { Pergunta: "Qual dia de hoje?", Resposta: new Date().toLocaleDateString() },
+        { Pergunta: "Qual dia é hoje?", Resposta: new Date().toLocaleDateString() },
+        { Pergunta: "dia de hoje?", Resposta: new Date().toLocaleDateString() }
+    ],
+
+    PerguntasTempo: [
+        { Pergunta: "Qual o clima?", Resposta: "teste" },
+    ],
+
+    ConsultarPergunta: function (pergunta) {
+
+        // Criei o objeto Retornos para ficar mais fácil de relacionar e filtrar as perguntas a partir do Banco de Dados
+
+        var Retornos = {
+            RetornoData: BancoDeDados.PerguntasData.filter(p => p.Pergunta == pergunta),
+            RetornoTempo: BancoDeDados.PerguntasTempo.filter(p => p.Pergunta == pergunta),
+        }
+
+        var dataHoje = new Date();
+        var divResposta = document.getElementById("resposta_enviada");
+        
+        //const perguntaRetornoData = BancoDeDados.PerguntasData.filter(p => p.Pergunta == pergunta);
+
+        /*if (Retornos.RetornoData.length == 0) {
+            alert("Desculpe, não tenho a resposta para esta pergunta");
+        } else {
+            alert(Retornos.RetornoData[0].Resposta);
+            divResposta.innerText = dataHoje.toLocaleDateString();
+        }
+
+        if (Retornos.RetornoTempo.length == 0) {
+            alert("Desculpe, não tenho a resposta para esta pergunta");
+        } else {
+            alert(Retornos.RetornoTempo[0].Resposta);
+            divResposta.innerText = Retornos.RetornoTempo[0].Resposta
+        */
+        
+        if (Retornos.RetornoData.length > 0) {
+            alert(Retornos.RetornoData[0].Resposta);
+            divResposta.innerText = dataHoje.toLocaleDateString();
+        }
+
+        if (Retornos.RetornoTempo.length > 0) {
+            alert(Retornos.RetornoTempo[0].Resposta);
+            divResposta.innerText = Retornos.RetornoTempo[0].Resposta
+        }
+    }
+}
+
+function checkAdult(perguntas, pergunta) {
+    return age >= 18;
+}
 
 var Util = {
     EscreverNomeDiv: function () { }
 }
+
